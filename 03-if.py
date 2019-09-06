ed446ae1175de88ba78d7bf0b58163e8881c5a63
Mike. Shi <1019726435@qq.com>
2019-09-06 23:06:52 +0800

    updated

--- 03-if.py
+++ 03-if.py
@@ -18,10 +18,10 @@ else:
 # 内嵌 if 语句
 num = float(input("输入一个数字: "))
 if num >= 0:
-   if num == 0:
-       print("零")
-   else:
+   if num > 0:
        print("正数")
+   else:
+       print("零")
 else:
    print("负数")
 
