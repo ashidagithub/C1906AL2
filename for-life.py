71f7ca867f1f763c128698f9873bdc451980bc8c
Mike. Shi <1019726435@qq.com>
2019-09-07 11:06:24 +0800

    updated

--- for-life.py
+++ for-life.py
@@ -3,23 +3,19 @@
 
 # Weekly life at Mon. to Fri.
 for myday in range(1,6):
+    print('Week schedule------------')
     print('Weekday (%d) is a class day !' % myday)
 
     # daily life at Mon. to Fri.
+    for mytime in range(0,7):
+        action = '  %02dClock... Sleeping' % mytime
+        print(action)
     for mytime in range(7,17):
         action = '  %02dClock... Study at school' % mytime
         print(action)
 
-    for mytime in range(17,23):
+    for mytime in range(17,25):
         action = '  %02dClock... Study at Home' % mytime
-        print(action)
-
-    for mytime in range(23,24):
-        action = '  %02dClock... Sleeping' % mytime
-        print(action)
-
-    for mytime in range(0,7):
-        action = '  %02dClock... Sleeping' % mytime
         print(action)
 
 # Life for weekend
